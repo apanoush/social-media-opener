59c7528a839a7d41f26203d6ab38dad0c28ff7b8
anoush <dakeon@outlook.fr>
2022-09-04 21:42:19 +0200

    5

--- social-media-opener.scala
+++ social-media-opener.scala
@@ -6,21 +6,17 @@ import scala.io.StdIn.readLine
 
 class ReseauxSociaux(url: String, choice: Int) {
     
-    def make_instagram: String = {
-        "https://instagram.com/".+(this.url)
-    }
-    def make_twitter: String = {
-        "https://twitter.com/".+(this.url)
-    }
-    def make_linktree: String = {
-        "https://linktr.ee/".+(this.url)
-    }
+    def make_instagram: String = "https://instagram.com/".+(this.url)
+    def make_twitter: String = "https://twitter.com/".+(this.url)
+    def make_pinterest: String = "https://pinterest.com/" + this.url
+    def make_linktree: String = "https://linktr.ee/".+(this.url)
     
     def makeURL: String = {
         this.choice match {
         case 1 => this.make_instagram
         case 2 => this.make_twitter
-        case 3 => this.make_linktree
+        case 3 => this.make_pinterest
+        case 4 => this.make_linktree
         case _ => "erreur"}
     }
 }
@@ -36,7 +32,7 @@ object main {
         }
 
         def input2: Int = {
-            println("1. instagram" ) ; println("2. twitter") ; println("3. linktree") ; println("veuillez choisir (1 à 3): ")
+            println("1. instagram" ); println("2. twitter"); println("3. pinterest"); println("4. linktree"); println("veuillez choisir (1 à 4): ")
             readLine.toInt
         }
 
